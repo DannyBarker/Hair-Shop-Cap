455f007b3269b20ccc5477ecada9273e03d60ad5
Danny Barker <barkerdanny17@yahoo.com>
2019-07-29 20:04:59 -0500

    got search bar for admin users loading results to page

--- src/components/admin/users/UserCard.js
+++ src/components/admin/users/UserCard.js
@@ -7,9 +7,7 @@ export default class UserCard extends Component {
   render() {
     return (
       <div key={this.props.user.id} className="user-card">
-        <h5 className="user-name">{`${this.props.user.firstName} ${
-          this.props.user.lastName
-        }`}</h5>
+        <h5 className="user-name">{this.props.user.name}</h5>
         <p className="user-email">{this.props.user.email}</p>
         <button
           onClick={() => {
--- src/components/admin/users/Users.js
+++ src/components/admin/users/Users.js
@@ -1,29 +1,75 @@
-import React, {Component} from 'react';
-import UserCard from './UserCard'
-import './Users.css'
-import "bootstrap/dist/css/bootstrap.min.css"
+import React, { Component } from "react";
+import UserCard from "./UserCard";
+import "./Users.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class Users extends Component {
+state = {
+  userResults: [],
+  searchInput: false
+}
 
-  userDetails = (id) => {
-    this.props.history.push(`/admin/user/${id}/details`)
-  }
+
+  inputEvent = event => {
+    const newState = {};
+    if (event.key === "Enter") {
+      let input = document.getElementById("searchUsers").value;
+      console.log();
+      fetch(`http://localhost:5002/users?name_like=${input}`)
+        .then(r => r.json())
+        .then(users => this.setState({
+          searchInput: true,
+          userResults: users
+        }))
+    }
+  };
+
+  userDetails = id => {
+    this.props.history.push(`/admin/user/${id}/details`);
+  };
 
   render() {
     return (
       <React.Fragment>
-      <header className="adminUsers-header">
-        <h1>Users</h1>
-        <input type="text" placeholder="Search Users" />
-      </header>
-      <div className="adminUsers-Div">
-        {
-          this.props.users.map( user => user.accessTypeId === 2 ? <UserCard key={user.id} user={user} userDetails={this.userDetails} /> : "")
-        }
-      </div>
-
-
+        <header className="adminUsers-header">
+          <h1>Users</h1>
+          <input
+            id="searchUsers"
+            type="text"
+            placeholder="Search Users"
+            onKeyPress={this.inputEvent}
+          />
+        </header>
+        {!this.state.searchInput ? (
+          <div className="adminUsers-Div">
+            {this.props.users.map(user =>
+              user.accessTypeId === 2 ? (
+                <UserCard
+                  key={user.id}
+                  user={user}
+                  userDetails={this.userDetails}
+                />
+              ) : (
+                ""
+              )
+            )}
+          </div>
+        ) : (
+          <div className="adminUsersSearch-Div">
+            {this.state.userResults.map(user =>
+              user.accessTypeId === 2 ? (
+                <UserCard
+                  key={user.id}
+                  user={user}
+                  userDetails={this.userDetails}
+                />
+              ) : (
+                ""
+              )
+            )}
+          </div>
+        )}
       </React.Fragment>
-    )
+    );
   }
 }
